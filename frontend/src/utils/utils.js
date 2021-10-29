f1f621f42eba9a41605484e21d156e095e4cd977
Logan Schmitt <logan.schmitt@me.com>
2021-10-28 20:42:59 -0500

    Added 'createUser' route and api folder for frontend

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,25 @@
+import axios from 'axios';
+
+
+// ENTER YOUR EC2 PUBLIC IP/URL HERE
+const ec2_url = ''
+// CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
+const ec2 = false;
+// USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
+const url = ec2 ? ec2_url : 'localhost'
+
 export const isLogin = () => {
     // add functionality to see if logged in
     return true
+}
+
+export const signup = (values) => {
+    axios.get(`http://${url}:8000/createuser`, {
+        firstname: values.firstName,
+    }).then(
+      res => {
+        console.log(res);
+    }).catch(err => {
+      console.log(err)
+    });
 }
