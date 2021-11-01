7184dab22e382f4881507a0c257f722a42276d3c
cgalante1 <78107933+cgalante1@users.noreply.github.com>
2021-11-01 12:54:15 -0500

    DeleteEvent Route using event name and date

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -38,6 +38,40 @@ router.post("/createEvent", async (req, res) => {
       connection.release();
     });
   });
+
+  //Delete event route using event name and date
+  router.delete("/deleteEvent", async (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+        console.log(connection);
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error("Problem obtaining MySQL connection", err);
+        res.status(400).send("Problem obtaining MySQL connection");
+      } else {
+        let eventName = req.body['Event_Name'];
+        let eventDate = req.body['Event_Date'];
+    // if there is no issue obtaining a connection, execute query
+        connection.query(
+          "DELETE FROM events WHERE Event_Name = ? &&  Event_Date = ?",
+          [eventName, eventDate],
+          (err, rows, fields) => {
+            if (err) {
+              logger.error("Error while deleting event\n", err);
+              res.status(400).json({
+                data: [],
+                error: "Error deleting values",
+              });
+            } else {
+              res.status(200).json({
+                data: rows,
+              });
+            }
+          }
+        );
+      }
+      connection.release();
+    });
+  });
   
   module.exports = router;
 
