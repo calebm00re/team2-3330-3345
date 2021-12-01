c5be1e8abbf7517e89264fde021f8033f8d9eef7
Logan Schmitt <logan.schmitt@me.com>
2021-12-01 13:29:42 -0600

    Deleted unecessary in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,66 +31,7 @@ function App () {
   const ec2 = false;
   // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
   const url = ec2 ? ec2_url : 'localhost'
-
-  // handle input field state change
-  const handleChange = (e) => {
-    setNumber(e.target.value);
-  }
-
-  const fetchBase = () => {
-    axios.get(`http://${url}:8000/`).then((res)=>{
-      alert(res.data);
-    })
-  }
-
-  // fetches vals of db via GET request
-  const fetchVals = () => {
-    axios.get(`http://${url}:8000/values`).then(
-      res => {
-        const values = res.data.data;
-        console.log(values);
-        setValues(values)
-    }).catch(err => {
-      console.log(err)
-    });
-  }
-
-  // handle input form submission to backend via POST request
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let prod = number * number;
-    axios.post(`http://${url}:8000/multplynumber`, {product: prod}).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });;
-    setNumber("");
-  }
-
-  // handle intialization and setup of database table, can reinitialize to wipe db
-  const reset = () => {
-    axios.post(`http://${url}:8000/signup`).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });;
-  }
   
-  // const signUp = () => {
-  //   axios.post(`http://${url}:8000/reset`).then(res => {
-  //     console.log(res);
-  //     fetchVals();
-  //   }).catch(err => {
-  //     console.log(err)
-  //   });;
-  // }
-
-  // tell app to fetch values from db on first load (if initialized)
-  useEffect(() => {
-    fetchVals();
-  }, [])
 
   return (
     <>
