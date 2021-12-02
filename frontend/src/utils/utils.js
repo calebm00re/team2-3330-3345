9d08792b18758817b8dfb2a50cf9c7a3d0099bbe
Logan Schmitt <logan.schmitt@me.com>
2021-12-02 13:56:16 -0600

    Update utils.js

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
-const ec2_url = '3.143.233.193:3000'
+const ec2_url = '3.143.233.193:8000'
 // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
 const ec2 = true;
 // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
