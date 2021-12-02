c84617cda7b3a1b9cf3360c9cce0b2a2005022c6
Logan Schmitt <logan.schmitt@me.com>
2021-12-02 14:14:33 -0600

    update ec2 url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import React from 'react';
 
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
-const ec2_url = '3.143.233.193:8000'
+const ec2_url = 'http://3.143.233.193:8000'
 // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
 const ec2 = true;
 // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
 export const URL = ec2 ? ec2_url : 'http://localhost:8000'
-
+// export const URL = "http://3.143.233.193:8000";
 export const isLogin = () => {
     // add functionality to see if logged in
     return true
